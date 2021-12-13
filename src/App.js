daeafb33b28cc57587a2f7c8e31e16910ceb6465
carina <c.malgut@googlemail.com>
2021-12-13 11:49:02 +0100

    change

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="coin-app">
+      <div className="coin-search">
+      <h1 className="coin-text">Search a currency</h1>
+       <form>
+        <input type="text" placeholder="Search" className="coin-input"/>
+
+       </form>
+    </div>
     </div>
   );
 }
